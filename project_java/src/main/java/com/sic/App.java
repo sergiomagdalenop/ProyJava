6f2a7c6b51bc5a36bea4c6feb5711313705890bd
Jose Sergio Magdaleno Palencia <95958997+sergiomagdalenop@users.noreply.github.com>
2022-05-18 15:42:56 -0700

    Ejemplo Mysql

--- project_java/src/main/java/com/sic/App.java
+++ project_java/src/main/java/com/sic/App.java
@@ -1,13 +1,9 @@
 package com.sic;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+          System.out.println("Hola mundo"); 
 }
+ 
